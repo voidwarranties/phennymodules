1997f3c4950c386a7e213e4738d476f492aa5a63
Ward De Ridder <git@warddr.eu>
2013-10-15 10:19:27 +0200

    first version of saywhat

---
+++ saywhat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+status.py - Phenny spacestatus module
+Author: Ward De Ridder
+"""
+
+def status(phenny, input): 
+	phenny.say("Don't be lazy, just read it again!")
+status.commands = ["saywhat"]
+
+if __name__ == '__main__': 
+   print __doc__.strip()
