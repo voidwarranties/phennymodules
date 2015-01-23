218c12ea71d3ab42d0ae771acbaa0cbcca457707
unloading <matt.braem@gmail.com>
2015-01-23 20:23:28 +0100

    Using json as storage system, fixed some bugs

--- voidwillbeopen.py
+++ voidwillbeopen.py
@@ -1,13 +1,9 @@
 """
 voidwillbeopen.py
-
 A module for willie http://willie.dftba.net/
-
 This module is used for getting information when our hackerspace will be open
 and the user who will open the doors.
-
 http://we.voidwarranties.be/
-
 Author: unloading
 """
 import willie.module
@@ -17,6 +13,8 @@ from willie.config import ConfigurationError
 import os
 import os.path
 from datetime import datetime
+
+import json
 
 def configure(config):
 	"""Adds settings to the config file"""
@@ -34,7 +32,7 @@ def get_file_path(bot, channel):
 	basedir = os.path.expanduser(bot.config.keylogs.dir)
 	channel = channel.lstrip("#")
 	dt = datetime.utcnow()
-	fname = "{channel}{date}.log".format(channel=channel,date=dt.date().isoformat())
+	fname = "{channel}{date}.json".format(channel=channel,date=dt.date().isoformat())
 	return os.path.join(basedir, fname)
 
 def setup(bot):
@@ -48,69 +46,142 @@ def setup(bot):
     if not os.path.exists(basedir):
         os.makedirs(basedir)
 
+def hour_range(start, stop, step):
+	li = []
+	while start <= stop:
+		li.append('%.2f' % start)
+		start += step
+	return li
 
-@willie.module.commands('go2vw')
-@willie.module.example('.go2vw @13.00, i have a key')
-def go2vw(bot, trigger):
+def updateDatabase(bot,userInfo,path,option):
+	if(option=="add"):
+		new_data_struct=[]
+		"""Read info from the .json file"""
+		with open(path,'a+') as f: 	#'r' not good enough, if file does not exists. Problems!
+			try:
+				j_data =json.load(f)
+				data_struct = json.loads(j_data)		
+				for i in range(len(data_struct)):
+					new_data_struct.append(data_struct[i])
+				new_data_struct.append(userInfo)
+			except ValueError:		#Build in ValueError , because if file empty => Problems !!! 
+				new_data_struct.append(userInfo)
+				
+		"""Write new info to the .json file"""
+		with open(path,'w') as f:
+			json.dump(json.dumps(new_data_struct), f)
+			bot.say("Your information has been stored.")
+			return
+
+	"""	Read info from .json file and search for entries 
+		with the username userInfo[0], delete those		"""
+	with open(path,'a+') as f:	#'r' not good enough, if file does not exists. Problems!
+		try:
+				j_data =json.load(f)
+		except ValueError:
+			bot.say("Wast of time!!!")
+			return
+		
+		data_struct = json.loads(j_data)
+		i=0
 	
-	hours = []
-	key_users = []
-	all_users = []
-	i=0
-	arrival_time = 25.00
-	key_found = False
+		while i < len(data_struct):
+			if data_struct[i][0] == userInfo[0]:
+				del data_struct[i]
+				print data_struct
+				i=0 #Should be optimalized !!!!!
+			if len(data_struct) != 1: #If jos was the only guy in the database, his entry would stay in the database. Not cool for jos.
+				i = i + 1
+	
+	with open(path,'w') as f:
+		json.dump(json.dumps(data_struct), f)
+		bot.say("Your information has been deleted.")
+		return
 
+def _decode_list(data):
+    rv = []
+    for item in data:
+        if isinstance(item, unicode):
+            item = item.encode('utf-8')
+        elif isinstance(item, list):
+            item = _decode_list(item)
+        elif isinstance(item, dict):
+            item = _decode_dict(item)
+        rv.append(item)
+    return rv
+
+@willie.module.commands('gvw')
+@willie.module.example('.gvw 13.00 key doorstatus attendies food')
+def gvw(bot, trigger):
+	key_found = False
+	valid_input_hour = False
 	message = trigger.group(2)
+	#dummy 
+	#userInfo = [username,has_key,needsfood,arrival_hour]
+	userInfo = [trigger.nick,False,False,None] 
+
+	#Fix bug if user types : .gvw with a space after it.
+	if message == None:
+		updateDatabase(bot,userInfo,get_file_path(bot, trigger.sender),None)
+		return		
+	
 	message = message.lower()
 	list_message = message.split()
+	hour = list_message[0].replace("@","")
+
+	"""Check all the variables, in the message. Construct userInfo[]"""
+	if hour in hour_range(0,24,0.01):
+		userInfo[3] = hour
+		if 'key' in message:		
+			userInfo[1] = True
+		if 'food' in message:
+			userInfo[2] = True
+		updateDatabase(bot,userInfo,get_file_path(bot, trigger.sender),"add")
+
+	"""Check when the door will open"""
 	
-	hour = " [HOUR] " + list_message[0].replace("@","")
-	user = " [USER] " + trigger.nick 
-	###################################################
-	##Register the info of sender.
-	###################################################
+	key_holder_user = []
+	key_holder_arrival = []
+	hungry_users_need_food = []
+	attending_users = []
+	key_found = False
 
-	if 'key' in message:
-		f_line = "[KEY]" + hour + user + "\n"
-		bot.say("Niobe: What is so important about that key?") 
-		bot.say("Keymaker: The key is integral to the path of the One.")
-	else:
-		f_line = hour + user + "\n"
+	with open(get_file_path(bot,trigger.sender),'a+') as f: 	#'r' not good enough, if file does not exists. Problems!
+		try:
+			j_data =json.load(f)
+			data_struct = json.loads(j_data,object_hook=_decode_list) #encoding not working atm????? Fix by line 154 and 155		
 
-	f=open(get_file_path(bot, trigger.sender), "a")
-	f.write(f_line)
-	f.close()
+			for i in range(len(data_struct)):
+				data_struct[i][0] = data_struct[i][0].encode('utf-8')
+				data_struct[i][3] = float(data_struct[i][3])
+				if data_struct[i][1] == True:
+					key_holder_user.append(data_struct[i][0])
+					key_holder_arrival.append(data_struct[i][3])
+					key_found = True
+				if data_struct[i][2] == True:
+					hungry_users_need_food.append(data_struct[i][0])
+				attending_users.append(data_struct[i][0])
 
-	###################################################
-	##Return the info about users and key to the sender.
-	###################################################
-	
-	f=open(get_file_path(bot, trigger.sender), "r")
-	for line in f:
-		splitline = line.split()
-		
+			arrival = 24.01
+			i=0                                       # Using this variable because its bigger than all the vars we can get.
+			position=0
+			
+			if key_found:
+				for item in key_holder_arrival:
+					if item < arrival:
+						arrival = item
+						position = i
+					i = i + 1
+				if 'doorstatus' in message:
+					bot.say("The door will be opened by %s at %.2f" 
+							% (key_holder_user[position],key_holder_arrival[position]))
+			if 'food' in message:
+				bot.say("The hungry citizens are %s" % hungry_users_need_food)
+			if 'attendies' in message:
+				bot.say("Attending: %s" % attending_users)
 
-		if 'KEY' in line:
-			hours.append(splitline[2])
-			key_users.append(splitline[4])
-			all_users.append(splitline[4])
-			key_found = True
-		else:
-			all_users.append(splitline[3])
+		except ValueError:										#Build in ValueError , because if file empty => Problems !!! 
+			bot.say("My database is empty!!!")
+			return
 
-	f.close()
-
-	if key_found:
-		for item in hours:
-			float_item = float(item)
-			if float_item < arrival_time:
-				arrival_time = float_item
-				position = i
-			i=i+1
-		bot.say("Voidwarranties will be open at %s (24.00 scale) , %s will open the doors" 
-				% (hours[position],key_users[position]))
-		bot.say("You will see the following users @voidwarranties: %s" % all_users)
-	else:
-		bot.say("Greetings keyless person, the keymaker has failed us.") 
-		bot.say("Maybe you will see the following users @voidwarranties: %s" % all_users)
 	
