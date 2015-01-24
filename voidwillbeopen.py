30a4223b44fc31a9c82f78707ba6e7ee9cf98020
Matthias Braem <unl0aD1NG@users.noreply.github.com>
2015-01-24 20:48:43 +0100

    Fixes. :/
    .gvw works now.

--- voidwillbeopen.py
+++ voidwillbeopen.py
@@ -1,7 +1,6 @@
 """
 voidwillbeopen.py
 A module for willie http://willie.dftba.net/
-
 http://we.voidwarranties.be/
 Author: unloading
 """
@@ -64,15 +63,17 @@ def updateDatabase(bot,userInfo,path,option):
 				for i in range(len(data_struct)):
 					new_data_struct.append(data_struct[i])
 				new_data_struct.append(userInfo)
-			except ValueError:		#Build in ValueError , because if file empty => Problems !!! 
+
+			except ValueError:		
+				print(ValueError)
 				new_data_struct.append(userInfo)
-
-		"""Write new info to the .json file"""
-		with open(path,'w') as f:
-			json.dump(json.dumps(new_data_struct), f)
-			bot.say("Your information has been stored.")
+			
+			"""Write new info to the .json file"""
+			with open(path,'w') as f:
+				json.dump(json.dumps(new_data_struct), f)
+				bot.say("Your information has been stored.")			
 			return
-
+			
 	"""	
 	Read info from .json file and search for entries 
 	with the username userInfo[0], delete those		
@@ -80,28 +81,22 @@ def updateDatabase(bot,userInfo,path,option):
 	if(option=="delete"):
 		with open(path,'a+') as f:	#'r' not good enough, if file does not exists. Problems!
 			try:
-				j_data =json.load(f)
+				j_data = json.load(f)
+				data_struct = json.loads(j_data)
+
+				for i in range(len(data_struct)):
+					if data_struct[i][0] == userInfo[0]:
+						del data_struct[i]
+		
+				with open(path,'w') as f:
+					json.dump(json.dumps(data_struct), f)
+					return
+
 			except ValueError:
 				bot.say("The database was empty?!")
 				return
 			
-			data_struct = json.loads(j_data)
-			i=0
-		
-			while i < len(data_struct):
-				#print data_struct[i][0]
-				if data_struct[i][0] == userInfo[0]:
-					del data_struct[i]
-					print data_struct
-					i = 0 #Should be optimalized !!!!!
-				if len(data_struct) != 1: #If jos was the only guy in the database, his entry would stay in the database. Not cool for jos.
-					i += 1
-		
-		with open(path,'w') as f:
-			json.dump(json.dumps(data_struct), f)
-			bot.say("Your information has been deleted.")
-			return
-
+			
 def findUserInDatabaseAndDelete(bot,userInfo,path):
 	with open(path,'a+') as f: 	#'r' not good enough, if file does not exists. Problems!
 		try:
@@ -112,8 +107,11 @@ def findUserInDatabaseAndDelete(bot,userInfo,path):
 			for i in range(len(data_struct)):
 				if userInfo[0] == data_struct[i][0]:
 					updateDatabase(bot,userInfo,path,"delete")
+			return
+
 		except ValueError:
 			return 
+
 
 def unicode_to_utf8(data):
 	if isinstance(data, list):
@@ -131,10 +129,11 @@ def gvw(bot, trigger):
 	message = trigger.group(2)
 	#dummy userInfo = [username,has_key,needsfood,arrival_hour]
 	userInfo = [trigger.nick,False,False,None] 
-
-	#Fix bug if user types : .gvw with a space after it.
-	if message == None:
+	
+	""".gvw deletes the information of the user"""
+	if message == None or message == '':
 		updateDatabase(bot,userInfo,get_file_path(bot, trigger.sender),"delete")
+		bot.say("Your information has been deleted.")
 		return		
 	
 	message = message.lower()
@@ -145,9 +144,10 @@ def gvw(bot, trigger):
 	if hour in hour_range(0,24,0.01):
 		userInfo[3] = float(hour)
 		if 'key' in message:		
-			userInfo[1] = True
+			userInfo[1] = True	
 		if 'food' in message:
 			userInfo[2] = True
+
 		findUserInDatabaseAndDelete(bot,userInfo,get_file_path(bot,trigger.sender))
 		updateDatabase(bot,userInfo,get_file_path(bot, trigger.sender),"add")
 	
@@ -157,11 +157,10 @@ def gvw(bot, trigger):
 	hungry_users_need_food = []
 	attending_users = []
 	key_found = False
-	#bot.say('line 161')
-	if ('doorstatus' or 'food' or 'attendies') in message:	
+	
+	if 'doorstatus' in message or 'food' in message or 'attendies' in message:
 		with open(get_file_path(bot,trigger.sender),'a+') as f: 	#'r' not good enough, if file does not exists. Problems!
 			try:
-				#bot.say('line 165')
 				j_data = json.load(f)
 				data_struct = json.loads(j_data)
 				data_struct = unicode_to_utf8(data_struct) 
@@ -180,7 +179,6 @@ def gvw(bot, trigger):
 				arrival = 24.01
 				i = 0                                       # Using this variable because its bigger than all the vars we can get.
 				position = 0
-				
 				if key_found:
 					for item in key_holder_arrival:
 						if item < arrival:
@@ -198,7 +196,5 @@ def gvw(bot, trigger):
 					bot.say("Attending: %s" % attending_users)
 
 			except ValueError:										#Build in ValueError , because if file empty => Problems !!! 
-				bot.say("My database is empty!!!")
+				bot.say("My database is empty?!")
 				return
-
-
