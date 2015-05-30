a5b54d44ff656b877ba0f032c7832f78fa37b762
Ward De Ridder <warddr@gmail.com>
2015-05-30 15:31:04 +0200

    now obsolete

--- spaceapiapi.py
+++
@@ -1,9 +0,0 @@
-import urllib2
-import json
-
-def SpaceapiOpen(apiURL):
-    u = urllib2.urlopen(apiURL)
-    jsonSpaceAPI = json.load(u)
-    u.close()
-    spacestatus = ""
-    return jsonSpaceAPI['state']['open']
